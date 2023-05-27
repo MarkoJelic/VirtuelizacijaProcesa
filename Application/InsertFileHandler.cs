8bd4a957b042b0a0350fbe5e37ef61914a41b101
MarkoJelic <jelic.marko.2002@gmail.com>
2023-05-27 09:59:21 +0200

    Added Application class library i klase i interfejsi u njemu.

---
+++ Application/InsertFileHandler.cs
@@ -0,0 +1,34 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class InsertFileHandler : IInsertFileHandler
+    {
+        private readonly ICommand insertFileCommand;
+
+        public InsertFileHandler(ICommand insertFileCommand)
+        {
+            this.insertFileCommand = insertFileCommand;
+        }
+
+        public FileManipulationResults InsertFile()
+        {
+            FileManipulationResults results = new FileManipulationResults();
+            try
+            {
+                insertFileCommand.Execute();
+            }
+            catch (Exception ex)
+            {
+                results.ResultType = ResultTypes.Failed;
+                results.ResultMessage = ex.Message;
+            }
+            return results;
+        }
+    }
+}
