42da020b521209453c65da52584163d246b1ef72
MarkoJelic <jelic.marko.2002@gmail.com>
2023-05-30 11:26:35 +0200

    Dodato parsiranje fajla u servisu i kreiranje objekata Load klase

--- Service/EstimateService.cs
+++ Service/EstimateService.cs
@@ -1,9 +1,13 @@
 ﻿using Application;
 using Common;
+using CsvHelper;
 using FileSystemManipulation;
+using Microsoft.VisualBasic.FileIO;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,12 +19,29 @@ namespace Service
     {
         MyDelegate Del;
         private string path;
+        IEnumerable<Load> objects;
 
         public EstimateService()
         {
             Del += GetValue;
             this.path = ConfigurationManager.AppSettings["path"];
             FileDirUtil.CheckCreatePath(path);
+            objects = CreateObjects(@"C:\Users\Marko\source\repos\VirtuelizacijaProcesa\Service\fileMeasurements.csv");
+        }
+
+        public IEnumerable<Load> CreateObjects(string csv_file_path)
+        {
+            //List<Load> objects = new List<Load>();
+
+            using (var reader = new StreamReader(csv_file_path))
+            {
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    var records = csv.GetRecords<Load>();
+                    return records;
+                }
+            }
+
         }
 
         public FileManipulationResults GetFiles(FileManipulationOptions options)
